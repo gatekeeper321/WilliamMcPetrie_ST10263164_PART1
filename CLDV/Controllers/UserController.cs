35cc2a117cc502d7b0e827ab611675688fc9527f
lab_services_student <lab_services_student@labVMH8OX>
2024-03-20 15:43:10 +0200

    Add project files.

---
+++ CLDV/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using CLDV.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CLDV.Controllers
+{
+    public class UserController : Controller
+    {
+        public tblUsers usertbl = new tblUsers();
+
+        [HttpPost]
+
+        public ActionResult About(tblUsers Users)
+        {
+            var result = tblUsers.insert_User(Users);
+            return RedirectToAction("Home", "About");
+        }
+        [HttpGet]
+
+
+        public IActionResult About()
+        {
+            return View(usertbl);
+        }
+    }
+}
---
+++ CLDV/Models/tblUsers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+using System.Data.SqlClient;
+
+namespace CLDV.Models
+{
+    public class tblUsers : Controller
+    {
+        public static string con_string = "Server=tcp:cldv10263164-sql-server.database.windows.net,1433;Initial Catalog=CLDV-Database;Persist Security Info=False;User ID=st10263164;Password=Dieg011748;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+
+        public static SqlConnection con = new SqlConnection(con_string);
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public int insertUser()
+        {
+            string sql = "INSERT INTO tblUsers (username, userSurname, user, userEmail)";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Name", .Name);
+            cmd.Parameters.AddWithValue("@Surname", .Surname);
+            cmd.Parameters.AddWithValue("@Email", .Email);
+            con.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowsAffected;
+        }
+        
+
+    }
+}
